acf62b1933f89b0583338eae066cb62c741686c7
Keertan2006 <keertanengineering@gmail.com>
2025-08-06 14:11:43 +0530

    Initial Django project setup with chai app

---
+++ DjangoProject/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    #return HttpResponse("Hello, This is our First Django Website");
+    return render(request, 'index.html');
+
+def about(request):
+    return HttpResponse("Hello, This is our About page");
+
+def contact(request):
+    return HttpResponse("Hello, This is our Contact page");
